15762810ffdc34dd40d2268a0cfb180389ffe057
SZKYTU-mac <szkycode@gmail.com>
2022-12-04 23:27:59 +0100

    spahetti and work fetch data from api" ;
    d a
    qwe

--- page.js
+++ page.js
@@ -1,26 +1,48 @@
 
-// const res = await fetch("http://127.0.0.1:5000/ip");
-// const json = await res.json();
-// console.log(json);
 
 const api_url =
     "http://127.0.0.1:5000/ip";
+
+const freeip = [];
+const blockip = [];
 
 async function getapi(url) {
 
     const response = await fetch(url);
 
-    var data = await response.json();
-    console.log(data);
-    if (response) {
-        hideloader();
+    const data = await response.json();
+    // console.log(data);
+    for (const status in data){
+        if (data[status][2] == 0){
+            blockip.push(data[status])
+        } else{
+            freeip.push(data[status])
+        }
     }
-    show(data);
 }
 getapi(api_url);
+
+function hideloader() {
+    document.getElementById('loading').style.display = 'none';
+}
 
 // function hideloader() {
 //     document.getElementById('loading').style.display = 'none';
 // }
 
-    document.getElementById("employees").innerHTML = tab;
+function show(data) {
+    let tab =
+        `<tr>
+          <th>ip</th>
+          <th>hostname</th>
+         </tr>`;
+
+    for (let r of freeip.list) {
+        tab += `<tr> 
+    <td>${r.ip} </td>
+    <td>${r.hn}</td>
+</tr>`;
+    }
+
+console.log(freeip);
+document.getElementById("test").innerHTML = tab;}
